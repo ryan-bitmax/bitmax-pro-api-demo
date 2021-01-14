1502e7a6f4be61fa1d235df4c2701ac430f4274e
“ryan-bitmax” <ryan.guo@bitmax.io>
2021-01-14 17:56:13 +0800

    update new APIs

--- python/BitMaxService.py
+++ python/BitMaxService.py
@@ -7,6 +7,7 @@ import requests
 # Local imports
 from util import *
 
+
 class BitMaxCli:
 
     def __init__(self, config):
@@ -16,17 +17,16 @@ class BitMaxCli:
         self.__apikey = btmx_cfg['apikey']
         self.__secret = btmx_cfg['secret']
         print("API configuration:",
-              "\n  --host",self.__host,
-              "\n  --group",self.__group,
-              "\n  --apikey",self.__apikey[:5].ljust(10, "*"),
-              "\n  --secret",self.__secret[:6].ljust(15, "*"),)
+              "\n  --host", self.__host,
+              "\n  --group", self.__group,
+              "\n  --apikey", self.__apikey[:5].ljust(10, "*"),
+              "\n  --secret", self.__secret[:6].ljust(15, "*"), )
 
     '''
     ======================
     Market data API
     ======================
     '''
-
     def query_pub_assets(self):
 
         url = f"{self.__host}/{ROUTE_PREFIX}/assets"
@@ -43,14 +43,14 @@ class BitMaxCli:
 
         url = f"{self.__host}/{ROUTE_PREFIX}/barhist"
         params = {
-            "symbol":   symbol,
+            "symbol": symbol,
             "interval": interval,
         }
-        if frm: params['frm']=frm
-        if to: params['to']=to
-        if n: params['n']=n
+        if frm: params['frm'] = frm
+        if to: params['to'] = to
+        if n: params['n'] = n
 
-        res = requests.get(url, params = params)
+        res = requests.get(url, params=params)
         return parse_response(res)
 
     def query_pub_depth(self, symbol):
@@ -75,4 +75,149 @@ class BitMaxCli:
         params = dict(symbol=symbol, n=n)
 
         res = requests.get(url, params=params)
+        return parse_response(res)
+
+    def query_pub_funding_rates(self, isApi=True, symbol='BTC-PERP', page=1, pageSize=20):
+        url = f"{self.__host}/{ROUTE_PREFIX}/futures/funding-rates" if isApi \
+            else f"{self.__host}/api/t/futures/funding-rates"
+        params = dict(symbol=symbol,
+                      page=page,
+                      pageSize=pageSize)
+
+        res = requests.get(url, params=params)
+        return parse_response(res)
+
+    def query_pub_ref_price(self, isApi=True, symbol=None):
+        url = f"{self.__host}/{ROUTE_PREFIX}/futures/ref-px" if isApi else f"{self.__host}/api/t/futures/ref-px"
+        params = dict()
+        if symbol: params['symbol'] = symbol
+        res = requests.get(url, params=params)
+        return parse_response(res)
+
+    def query_pub_market_data(self, isApi=True, symbol=None):
+        url = f"{self.__host}/{ROUTE_PREFIX}/futures/market-data" if isApi else f"{self.__host}/api/t/futures/market-data"
+        params = dict()
+        if symbol: params['symbol'] = symbol
+        res = requests.get(url, params=params)
+        return parse_response(res)
+
+    '''
+    ======================
+    Order API
+    ======================
+    '''
+    def order_new(self, account, symbol, price, qty, order_type, side, resp_inst, time_in_force):
+        url = f"{self.__host}/{self.__group}/api/pro/v1/{account}/order"
+
+        ts = utc_timestamp()
+
+        order = dict(
+            id=uuid32(),
+            time=ts,
+            symbol=symbol.replace("-", "/"),
+            orderPrice=str(price),
+            orderQty=str(qty),
+            orderType=order_type,
+            side=side.lower(),
+            timeInForce=time_in_force,
+            respInst=resp_inst,
+        )
+
+        headers = make_auth_headers(ts, "order", self.__apikey, self.__secret)
+        res = requests.post(url, headers=headers, json=order)
+        return parse_response(res)
+
+    def order_query(self):
+        pass
+
+    def order_cancel(self):
+        pass
+
+    def order_cancel_all(self, account="cash", symbol=None):
+
+        method = "order/all"
+
+        url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}/{method}"
+
+        params = {}
+        if symbol:  params["symbol"] = symbol
+
+        ts = utc_timestamp()
+        headers = make_auth_headers(ts, method, self.__apikey, self.__secret)
+
+        res = requests.delete(url, headers=headers, json=params)
+        return parse_response(res)
+
+    def query_prv_order_hist_v2(self, account="cash", symbol=None, start_time=None, end_time=None, seq_num=None,
+                                limit=None):
+
+        url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX_V2}/order/hist"
+
+        ts = utc_timestamp()
+        headers = make_auth_headers(ts, "order/hist", self.__apikey, self.__secret)
+        params = dict(
+            account=account,
+        )
+        if symbol: params['symbol'] = symbol
+        if start_time: params['startTime'] = start_time
+        if end_time: params['endTime'] = end_time
+        if seq_num: params['seqNum'] = seq_num
+        if limit: params['limit'] = limit
+
+        res = requests.get(url, headers=headers, params=params)
+        return parse_response(res)
+
+    def get_hist_orders(self, symbol, start_time, end_time, order_type, side, account="cash",
+                        method="order/hist/current"):
+        base_url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}"
+        ts = utc_timestamp()
+        url = "{}/{}".format(base_url, method)
+        headers = make_auth_headers(ts, method, self.__apikey, self.__secret)
+        params = {"symbol": symbol, "startTime": start_time, "endTime": end_time, "orderType": order_type, "side": side}
+        res = requests.get(url, headers=headers, params=params)
+        return parse_response(res)
+
+    def get_open_orders(self,  symbol, account="cash", method="order/open"):
+        base_url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}"
+        ts = utc_timestamp()
+        url = "{}/{}".format(base_url, method)
+        headers = make_auth_headers(ts, method, self.__apikey, self.__secret)
+        params = {"symbol": symbol}
+        res = requests.get(url, headers=headers, params=params)
+        return parse_response(res)
+
+    def get_order_status(self, order_id, account="cash", method="order/status"):
+        base_url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}"
+        url = "{}/{}".format(base_url, method, order_id)
+        ts = utc_timestamp()
+        headers = make_auth_headers(ts, method, self.__apikey, self.__secret)
+        res = requests.get(url, headers=headers, params={"orderId": order_id})
+        return parse_response(res)
+
+    '''
+    ======================
+    Account API
+    ======================
+    '''
+
+    def query_prv_account_info(self):
+        ts = utc_timestamp()
+        headers = make_auth_headers(ts, "info", self.__apikey, self.__secret)
+        url = f"{self.__host}/{ROUTE_PREFIX}/info"
+
+        res = requests.get(url, headers=headers)
+        return parse_response(res)
+
+    def query_prv_balance(self, asset=None, account='cash', show_all=False):
+        ts = utc_timestamp()
+        if account=="futures":
+            headers = make_auth_headers(ts, "futures/collateral-balance", self.__apikey, self.__secret)
+            url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}/collateral-balance"
+        else:
+            headers = make_auth_headers(ts, "balance", self.__apikey, self.__secret)
+            url = f"{self.__host}/{self.__group}/{ROUTE_PREFIX}/{account}/balance"
+        params = dict(showAll=show_all)
+        if asset: params['asset'] = asset
+
+        res = requests.get(url, headers=headers, params=params)
         return parse_response(res)
--- python/client_demo.py
+++ python/client_demo.py
@@ -5,17 +5,67 @@
 
 from BitMaxService import BitMaxCli
 from pprint import pprint
+from itertools import count
+from time import sleep
+import threading
+import time
 
-cli=BitMaxCli("../config_prod.json")
+cli=BitMaxCli("../config_staging.json")
 
 # pprint(cli.query_pub_assets())
 #
 # pprint(cli.query_pub_products())
-
-# pprint(cli.query_pub_barhist("BTC/USDT","1",n=2))
-
+#
 # pprint(cli.query_pub_depth("BTC/USDT"))
-
+#
 # pprint(cli.query_pub_ticker("BTC/USDT"))
-
+#
 # pprint(cli.query_pub_trades("BTC/USDT", 1))
+
+# pprint(cli.order_cancel_all())
+
+# for i in range(70):
+#     pprint(cli.order_new("cash", "BTC/USDT", "7200", "0.001", "limit", "sell", "ACCEPT", "GTC"))
+
+# pprint(cli.get_open_orders(symbol='BTC/USDT'))
+
+# pprint(cli.get_order_status(order_id='r176f5fef88cU3792951278sbtcukv94'))
+
+# pprint(cli.query_prv_order_hist_v2(symbol='BTC-PERP'))
+
+# pprint(cli.query_prv_account_info())
+#
+# pprint(cli.query_prv_balance())
+
+def test_apis(id):
+    print(f"{id} starts calling APIs ...")
+    for i in count(1):
+        print(id, 'iter', i)
+        cli.query_prv_order_hist_v2()
+        # cli.query_pub_market_data()['code']
+        # cli.query_pub_market_data(isApi=False, symbol='BTC-PERP')
+        # cli.query_pub_funding_rates(isApi=False, symbol='BTC-PERP', page=1, pageSize=1)
+        cli.query_pub_funding_rates(symbol='BTC-PERP', page=1, pageSize=1)
+        # cli.query_pub_ref_price()
+        # cli.query_pub_ref_price(isApi=False)
+        cli.query_pub_assets()
+        cli.query_pub_products()
+        cli.order_new("cash", "BTC/USDT", "7100", "0.001", "limit", "buy", "ACCEPT", "GTC")
+        cli.query_pub_depth("BTC/USDT")
+        cli.query_pub_ticker("BTC/USDT")
+        cli.query_pub_trades("BTC/USDT", 1)
+        orderId=cli.order_new("cash", "BTC/USDT", "7200", "0.001", "limit", "sell", "ACCEPT", "GTC")['data']['info']['orderId']
+        cli.get_open_orders(symbol='BTC/USDT')
+        cli.order_cancel_all()
+        cli.get_order_status(order_id=orderId)
+        cli.query_prv_account_info()
+        cli.query_prv_balance()
+        sleep(0.1)
+        i+=1
+
+try:
+    for j in range(20):
+        threading.Thread(target=test_apis, args=("thread %d"%j,)).start()
+        sleep(0.2)
+except:
+    print("Error: unable to start thread")
